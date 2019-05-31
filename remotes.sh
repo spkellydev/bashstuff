ac181e76e038650b4d24a793cc19ff98b2a52173
Sean Kelly <skelly@aaski.com>
2019-05-31 13:19:13 -0400

    rrfis primary / secondary remotes

--- remotes.sh
+++ remotes.sh
@@ -14,12 +14,40 @@ function openrdp() {
   mstsc "$@" &
 }
 
+function rrfis_rdp() {
+  local CHOICE=
+  PS3='Which RRFIS system do you need? '
+  options=("Primary (default)" "Secondary")
+  select opt in "${options[@]}"
+  do
+    case $opt in
+      "Primary")
+        CHOICE="primary"
+        break
+        ;;
+      "Secondary")
+        CHOICE="secondary"
+        break
+        ;;
+      *) CHOICE="primary"
+        break
+        ;;
+    esac
+  done
+  echo $CHOICE
+}
+
 # Remote Desktop Connection
 function rdp() {
   ARGS=
   if [[ $1 ]]; then
+    SSYS="$(echo $1 | tr a-z A-Z)"
+    if [[ $SSYS == 'RRFIS' ]]; then
+      SSYS="RRFIS-$(rrfis_rdp)"
+      echo "$SSYS"
+    fi
     # Create the connection file name, uppercase is important
-    REMOTE="$(echo $1 | tr a-z A-Z).rdp"
+    REMOTE="$SSYS.rdp"
     RDP_CONNECTIONS=~/Documents/Locker/Info/rdp
     pushd -- $RDP_CONNECTIONS > /dev/null
     if [[ -f "$RDP_CONNECTIONS/$REMOTE" ]]; then
